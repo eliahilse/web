fd15e1b683bd3791099563d3f9bb4f5b8eee5f3b
Elia <elia@eliahilse.com>
2024-11-22 21:10:47 +0100

    fix: text

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -11,10 +11,10 @@ export default function Home() {
           <path strokeLinecap="round" strokeLinejoin="round" d="m21 7.5-2.25-1.313M21 7.5v2.25m0-2.25-2.25 1.313M3 7.5l2.25-1.313M3 7.5l2.25 1.313M3 7.5v2.25m9 3 2.25-1.313M12 12.75l-2.25-1.313M12 12.75V15m0 6.75 2.25-1.313M12 21.75V19.5m0 2.25-2.25-1.313m0-16.875L12 2.25l2.25 1.313M21 14.25v2.25l-2.25 1.313m-13.5 0L3 16.5v-2.25" />
         </svg>
 
-        <h1 className="z-20 relative text-2xl font-bold">Coming Soon™</h1>
+        <h1 className="z-20 relative text-2xl font-bold">Work in Progress™</h1>
         <p className="z-20 relative mt-4">In the meantime, you can visit my
-          <a href="https://eliahilse.com" className="inline-flex items-center justify-center cursor-pointer">
-            <span className="underline ml-1 mr-1">old website</span>
+          <a href="https://x.com/eliahilse" className="inline-flex items-center justify-center cursor-pointer">
+            <span className="underline ml-1 mr-1">X</span>
             <ExternalLink className="w-5 h-5"/>.
           </a>
         </p>
